07bd047aea141dec5446a15b2a4ffe3877d053ae
fedyaaaaa <loreta.razalinska@gmail.com>
2025-04-24 15:00:30 +0300

    more obsticles

---
+++ SkiGame/Assets/Scripts/Snowball.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snowball : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+    }
+}
